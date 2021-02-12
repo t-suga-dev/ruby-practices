7f3f3447cdae4b057ec054cb9ffd99c0c78eab16
t-suga <t-suga@ruby-dev.jp>
2021-02-12 10:11:28 +0900

    月と年を１行のコードに変更、曜日と日付の位置コード短縮、rjust実装コード短縮

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -8,13 +8,12 @@ elsif ARGV[0] == "-y"
   dates = Date.new(ARGV[1].to_i, dates.month) 
 elsif ARGV[0] == "-m"
   dates = Date.new(dates.year, ARGV[1].to_i)
-else ARGV[0] == nil or /.*/
+else ARGV[0] == nil
   dates
 end
 
 # カレンダー上部の月、年、曜日を表示
-print ("      #{dates.month}月 ")
-puts dates.year
+puts ("      #{dates.month}月 ") + "#{dates.year}"
 puts (" 日 月 火 水 木 金 土")
 
 
@@ -24,40 +23,20 @@ last_day = Date.new(dates.year, dates.month, -1)
 first_day = Date.new(dates.year, dates.month, 1)
 
 # 最初の週の曜日と日付の位置を合わせる設定
-if first_day.wday == 0
-  print ""
-elsif
-  first_day.wday == 1
+first_day.wday.times do |i|
   print "   "
-elsif
-  first_day.wday == 2
-  print "      "
-elsif
-  first_day.wday == 3
-  print "         "
-elsif
-  first_day.wday == 4
-  print "            "
-elsif
-  first_day.wday == 5
-  print "               "
-elsif
-  first_day.wday == 6
-  print "                  "
-end
+end 
 
 # その月の日数を計算
 days = [*first_day..last_day]
 
-# 土曜日が来る毎に折り返し表示する、日にちが一桁のところは空白を入れる
+# 土曜日が来る毎に折り返し表示する
 days.each do |i|
   if i.wday == 6 && i.day.to_s.length == 1
-    puts ("  #{i.day}")
-  elsif i.day.to_s.length == 1
-    print ("  #{i.day}")
+    puts (" #{i.day.to_s.rjust(2)}")
   elsif i.wday == 6
-    puts (" #{i.day}")
+    puts (" #{i.day.to_s.rjust(2)}")
   else
-    print (" #{i.day}")
+    print (" #{i.day.to_s.rjust(2)}")
   end
 end
