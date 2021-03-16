e0f534a96b40d776039123311730ac2c34ca0be8
t-suga <t-suga@ruby-dev.jp>
2021-03-16 17:19:12 +0900

    メソッド名の変更、処理開始部分を組み合わせ爆発が起きないように変更等

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,89 +4,60 @@ require 'etc'
 require 'optparse'
 
 # lオプションがないときの処理
-def ls_l_other(files)
+def max_word_count(files)
   # 要素の中で一番大きい文字数を持つものを抽出
   max_element = files.map(&:length).max
 
-  # 要素を左寄せにする
   files.map do |element|
     element.ljust(max_element)
   end
 end
 
-def ls(files)
-  # 要素の数を算出
-  element_num = ls_l_other(files).length
+COLUMN_COUNT = 3
+def process_ls(files)
+  file_count = files.count
 
   # 要素の数をもとに一列にいくつの要素が並ぶかを計算
-  column = if (element_num % 3).zero?
-             element_num / 3
-           else
-             element_num / 3 + 1
-           end
+  column = (file_count.to_f / COLUMN_COUNT).ceil
 
-  # 要素を上で計算した要素の数で切り分ける
-  slice_elements = []
-  ls_l_other(files).each_slice(column) do |left_element|
-    slice_elements << left_element
+  sliced_files = max_word_count(files).each_slice(column).to_a
+
+  blank_count = sliced_files[0].length - sliced_files[COLUMN_COUNT - 1].length
+  blank_count.times do
+    sliced_files[COLUMN_COUNT - 1] << ''
   end
 
-  # 要素の数が一緒ではない時空白を作る
-  unless slice_elements[0].length == slice_elements[2].length
-    blank_num = slice_elements[0].length - slice_elements[2].length
-    blank_num.times do
-      slice_elements[2] << ''
-    end
-  end
-
-  # 行と列の入れ替え
-  swap_elements = slice_elements.transpose
-
-  # 要素同士をつなげる
-  swap_elements.each do |element|
+  sliced_files.transpose.each do |element|
     puts element.join('  ')
   end
 end
 
 # lオプションがあるときの処理
-# ブロックサイズを算出
 def block_size(files)
-  blocksize = 0
-  files.each do |file|
-    blocksize += File.stat(file).blocks
-  end
-  blocksize
+  files.sum { |file| File.stat(file).blocks }
 end
 
-# ファイルタイプを識別する
 def file_type(ftype)
-  if ftype == 'file'
-    '-'
-  else
-    'd'
-  end
+  ftype == 'file' ? '-' : 'd'
 end
 
 def file_details(file)
-  file_type(File.stat(file).ftype)
+  file_type(file.ftype)
 end
 
 # パーミッション識別のためにファイルモードにして８進数にする
 def octal(file)
-  File.stat(file).mode.to_s(8)
+  file.mode.to_s(8)
 end
 
-# パーミッション所有者の権限を識別
 def owner(file)
   permission_judge(octal(file)[-3].to_i)
 end
 
-# パーミッション所有グループの権限を識別
 def group(file)
   permission_judge(octal(file)[-2].to_i)
 end
 
-# パーミッションその他のユーザーの権限を識別
 def other(file)
   permission_judge(octal(file)[-1].to_i)
 end
@@ -118,53 +89,48 @@ def permission_judge(octal)
   end
 end
 
-# ハードリンクを取得
 def hard_link(file)
-  File.stat(file).nlink
+  file.nlink
 end
 
-# ユーザー名
 def user_name(file)
-  Etc.getpwuid(File.stat(file).uid).name
+  Etc.getpwuid(file.uid).name
 end
 
-# グループ名
 def group_name(file)
-  Etc.getgrgid(File.stat(file).gid).name
+  Etc.getgrgid(file.gid).name
 end
 
-# ファイルの大きさ
-def bite_size(file)
-  File.stat(file).size
+def byte_size(file)
+  file.size
 end
 
-# 最終更新時刻
 def time(file)
-  File.stat(file).ctime.strftime('%-m %e %R')
+  file.ctime.strftime('%-m %e %R')
 end
 
 # 最終的なlオプションの結果を表示
-def ls_l(files)
+def process_ls_l(files)
   puts "total #{block_size(files)}"
   files.each do |file|
-    puts "#{file_details(file)}#{permission(file)}  #{hard_link(file)}  #{user_name(file)}   #{group_name(file)}  #{bite_size(file)}  #{time(file)}  #{file}"
+    stat = File.stat(file)
+    puts "#{file_details(stat)}#{permission(stat)}  #{hard_link(stat)}  #{user_name(stat)}   #{group_name(stat)}  #{byte_size(stat)}  #{time(stat)}  #{file}"
   end
 end
 
 # 処理開始
 option = ARGV.getopts('l', 'a', 'r')
-files = \
-  if option['a'] == true && option['r'] == true
-    Dir.entries('.').sort.reverse
-  elsif option['a'] == true && option['r'] == false
-    Dir.entries('.').sort
-  elsif option['a'] == false && option['r'] == true
-    Dir.glob('*').sort.reverse
+files =
+  if option['a']
+    Dir.glob('*', File::FNM_DOTMATCH).sort
   else
     Dir.glob('*').sort
   end
-if option['l'] == true
-  ls_l(files)
+
+files.reverse! if option['r']
+
+if option['l']
+  process_ls_l(files)
 else
-  ls(files)
+  process_ls(files)
 end
