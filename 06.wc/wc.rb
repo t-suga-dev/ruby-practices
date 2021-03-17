13cb8b5ecc6e0d59e6c653a8288c3a338a9200a3
t-suga <t-suga@ruby-dev.jp>
2021-03-17 13:33:48 +0900

    パイプあり、なしでメソッドを分けていたのを共通化

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -2,36 +2,16 @@
 
 require 'optparse'
 
-# 行数を数える(パイプなし)
 def string_count(file)
-  File.read(file).count("\n")
-end
-
-# 単語を数える(パイプなし)
-def word_count(file)
-  files = File.read(file)
-  word = files.split(/\s+/).size
-  word.size
-end
-
-# バイト数を数える(パイプなし)
-def bite_size(file)
-  File.stat(file).size
-end
-
-# 行数を数える(パイプあり)
-def pipe_string_count(file)
   file.count("\n")
 end
 
-# 単語数を数える(パイプあり)
-def pipe_word_count(file)
+def word_count(file)
   word = file.split(/\s+/)
   word.size
 end
 
-# バイト数を数える(パイプあり)
-def pipe_load_bytes(file)
+def bite_size(file)
   file.bytesize
 end
 
@@ -45,25 +25,29 @@ if File.pipe?($stdin) == false
   bite_size = 0
   if option['l'] == false
     files.each do |file|
+      file_name = file.dup
+      file = File.read(file)
       string_count += string_count(file)
       word_count += word_count(file)
       bite_size += bite_size(file)
-      puts "#{string_count(file).to_s.rjust(8)}#{word_count(file).to_s.rjust(8)}#{bite_size(file).to_s.rjust(8)} #{file}"
+      puts "#{string_count(file).to_s.rjust(8)}#{word_count(file).to_s.rjust(8)}#{bite_size(file).to_s.rjust(8)} #{file_name}"
     end
     puts "#{string_count.to_s.rjust(8)}#{word_count.to_s.rjust(8)}#{bite_size.to_s.rjust(8)} total" if files.size >= 2
   else
     files.each do |file|
+      file_name = file.dup
+      file = File.read(file)
       string_count += string_count(file)
 
-      puts "#{string_count(file).to_s.rjust(8)} #{file}"
+      puts "#{string_count(file).to_s.rjust(8)} #{file_name}"
     end
     puts "#{string_count.to_s.rjust(8)} total" if files.size >= 2
   end
 else
   # パイプがあるときの処理
   input = $stdin.read
-  lines = pipe_string_count(input)
-  words = pipe_word_count(input)
-  bytes = pipe_load_bytes(input)
+  lines = string_count(input)
+  words = word_count(input)
+  bytes = bite_size(input)
   puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
 end
