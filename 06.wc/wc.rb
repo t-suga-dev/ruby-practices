3060190ed2e774f072eaba597bdf015786766237
t-suga <t-suga@ruby-dev.jp>
2021-03-18 09:18:32 +0900

    if文内からfalseを削除

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -17,13 +17,29 @@ end
 
 # 処理開始
 # パイプがあるかどうかを判定
-if File.pipe?($stdin) == false
+if File.pipe?($stdin)
+  # パイプがあるときの処理
+  input = $stdin.read
+  lines = string_count(input)
+  words = word_count(input)
+  bytes = bite_size(input)
+  puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
+else
   option = ARGV.getopts('l')
   files = ARGV
   string_count = 0
   word_count = 0
   bite_size = 0
-  if option['l'] == false
+  if option['l']
+    files.each do |file|
+      file_name = file.dup
+      file = File.read(file)
+      string_count += string_count(file)
+
+      puts "#{string_count(file).to_s.rjust(8)} #{file_name}"
+    end
+    puts "#{string_count.to_s.rjust(8)} total" if files.size >= 2
+  else
     files.each do |file|
       file_name = file.dup
       file = File.read(file)
@@ -33,21 +49,5 @@ if File.pipe?($stdin) == false
       puts "#{string_count(file).to_s.rjust(8)}#{word_count(file).to_s.rjust(8)}#{bite_size(file).to_s.rjust(8)} #{file_name}"
     end
     puts "#{string_count.to_s.rjust(8)}#{word_count.to_s.rjust(8)}#{bite_size.to_s.rjust(8)} total" if files.size >= 2
-  else
-    files.each do |file|
-      file_name = file.dup
-      file = File.read(file)
-      string_count += string_count(file)
-
-      puts "#{string_count(file).to_s.rjust(8)} #{file_name}"
-    end
-    puts "#{string_count.to_s.rjust(8)} total" if files.size >= 2
   end
-else
-  # パイプがあるときの処理
-  input = $stdin.read
-  lines = string_count(input)
-  words = word_count(input)
-  bytes = bite_size(input)
-  puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
 end
